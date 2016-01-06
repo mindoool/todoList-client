d17ad819b51bf22dc0d61be30c8a0de07f81ea16
Minseok Kim <min3482@snu.ac.kr>
2016-01-06 22:08:25 +0900

    main work is revised

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('ToDoList', ['ngMaterial','ui.router']);
+var app = angular.module('ToDoList', ['ngMaterial','ngMessages','ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     //
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,39 @@
 app.controller('MainController', ['$scope', 'storage', function ($scope, storage) {
     $scope.mainToDoList = storage.get('mainToDoList') || [];
-    $scope.mainNewTodo = "";
+    $scope.mainNewTodo = {
+        title: "",
+        items: [{name:''}],
+        due:null,
+        completed: false
+    };
 
     $scope.addMainToDo = function () {
-        $scope.mainToDoList.push({
-            title: $scope.mainNewTodo,
-            completed: false
-        });
+        $scope.mainToDoList.push($scope.mainNewTodo);
         storage.set('mainToDoList', $scope.mainToDoList);
-        $scope.mainNewTodo = "";
+        $scope.initialize();
     };
 
     $scope.deleteMainToDo = function (maintodo) {
         var index = $scope.mainToDoList.indexOf(maintodo);
         $scope.mainToDoList.splice(index, 1);
         storage.set('mainToDoList', $scope.mainToDoList);
+    };
+
+    $scope.addItem = function () {
+        $scope.mainNewTodo.items.push({name:''})
+    };
+
+    $scope.initialize = function() {
+        $scope.mainNewTodo = {
+            title: "",
+            items: [{name:''}],
+            due:null,
+            completed: false
+        };
+    };
+
+    $scope.readonlyChange = function($scope) {
+        $scope.checked = true;
     };
 
     //$scope.deleteTodo = function($index) {
