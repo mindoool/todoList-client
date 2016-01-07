815570c23d1c22caca39042fddc452da7b9d4725
Minseok Kim <min3482@snu.ac.kr>
2016-01-07 23:35:00 +0900

    layout change, main work is revised

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -13,6 +13,8 @@ app.controller('MainController', ['$scope', 'storage', function ($scope, storage
         $scope.initialize();
     };
 
+
+
     $scope.deleteMainToDo = function (maintodo) {
         var index = $scope.mainToDoList.indexOf(maintodo);
         $scope.mainToDoList.splice(index, 1);
@@ -30,10 +32,6 @@ app.controller('MainController', ['$scope', 'storage', function ($scope, storage
             due:null,
             completed: false
         };
-    };
-
-    $scope.readonlyChange = function($scope) {
-        $scope.checked = true;
     };
 
     //$scope.deleteTodo = function($index) {
--- js/directives/main.js
+++ js/directives/main.js
@@ -4,6 +4,12 @@ app.directive('main', function(){
         scope: {
             maintodo: '='
         },
+        controller: ['$scope', function($scope) {
+            $scope.maintodo.due = new Date($scope.maintodo.due);
+            $scope.addItem = function () {
+                $scope.maintodo.items.push({name:''})
+            };
+        }],
         templateUrl: 'templates/directives/main.html'
     };
 });
