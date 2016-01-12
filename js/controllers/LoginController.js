6d80e26eb3ffe3273f2ee145ee76c741c54cfd71
Minseok Kim <min3482@snu.ac.kr>
2016-01-12 23:25:16 +0900

    signup modal

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -1,4 +1,4 @@
-app.controller('LoginController',['$scope','ApiService', function($scope, ApiService){
+app.controller('LoginController',['$scope','ApiService','$mdDialog', '$mdMedia', function($scope, ApiService, $mdDialog, $mdMedia){
     $scope.data = [];
     ApiService.getData(function(data){
         $scope.data = data.data;
@@ -9,4 +9,40 @@ app.controller('LoginController',['$scope','ApiService', function($scope, ApiSer
         password:""
     }
 
+    $scope.status = '  ';
+    $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+
+    $scope.showAdvanced = function(ev) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'templates/signup.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: useFullScreen
+        })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+        $scope.$watch(function() {
+            return $mdMedia('xs') || $mdMedia('sm');
+        }, function(wantsFullScreen) {
+            $scope.customFullscreen = (wantsFullScreen === true);
+        });
+    };
+
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+    }
 }]);
